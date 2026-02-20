39dc31cf1889a8ff8500d03a9bf36642c23a8d36
pyosj <pyosuejung@gmail.com>
2026-02-20 14:04:26 +0900

    [Bronze] 2750_수 정렬하기

---
+++ src/baekjoon/bronze/boj_2750.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class boj_2750 {
+    //수 정렬하기
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        //버블정렬 구현
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
